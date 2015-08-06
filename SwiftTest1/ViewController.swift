9184e64e4c34649eceab814491043006186338d8
Larry Feldman <larryfeldman@Larrys-MacBook-Pro.local>
2015-08-06 09:47:50 -0700

    Trying optionals

--- SwiftTest1/ViewController.swift
+++ SwiftTest1/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // here
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        var temperature : Int? // optional (int or nil)
+        
+        if temperature != nil {
+            
+            println("The temperature is \(temperature) degrees")  // string interpolation
+        }
+        
+        temperature = 10
+        
+        if temperature != nil {
+            
+            println("The temperature is \(temperature) degrees")  // prints "Optional in front
+        }
+        
+        if temperature != nil {
+            
+            println("The temperature is \(temperature!) degrees")  // forced unwrapping (! removes "optional")
+            
+            // Only do this if you're certain temperature is not nil
+
+        }
+        
+        var states = [ "AZ" : "Arizona", "CA" : "California"]
+        
+        var fullname1 = states["IL"]
+        
+        println("The state is \(fullname1)")
+
+        // Optional binding
+        
+        if let fullname = states["AZ"] {
+            
+            println("The state is \(fullname)")
+            
+        } else {
+            
+            println("No details found")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
